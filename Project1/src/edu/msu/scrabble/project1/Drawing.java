6a03ce61e6c34a6d4c75f157959835513c2e0f4f
Alex Seling <alex.seling@gmail.com>
2013-02-20 14:25:57 -0500

    Added drawing and FinalActivity classes

---
+++ Project1/src/edu/msu/scrabble/project1/Drawing.java
@@ -0,0 +1,66 @@
+package edu.msu.scrabble.project1;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Drawing implements Serializable {
+	
+	// the paint for the freehand drawing - color and width
+	public Paint linePaint = new Paint();
+	
+	// the location of a Drawing is a list of x and y coordinates
+	// that a touch passes through while drawing.
+	// to rotate the drawing, etc, each of these points must be changed
+	// according to the rotation, etc, function
+	public ArrayList<Point> points = new ArrayList<Point>();
+	
+	public class Point implements Serializable {
+		float x;
+		float y;
+		
+		Point(float a, float b) {
+			x = a;
+			y = b;
+		}
+	}
+	
+	// should scale and rotation variables be kept here?
+	
+	
+	// to draw a Drawing each of the coordinates in points
+	// and points must be connected in order by a line of
+	// 'color' and 'lineWidth'
+	public void DrawLine(Canvas canvas) {
+		// connect the points with a line of the specified color
+		// and width
+		/*for (int i=1; i<this.points.size(); i++) {
+			canvas.drawLine(this.points.get(i-1), this.points.get(i-1), 
+					this.points.get(i), this.points.get(i), linePaint);
+		}*/
+		for (int i=1; i<this.points.size(); i++) {
+			canvas.drawLine(this.points.get(i-1).x, this.points.get(i-1).y, 
+					this.points.get(i).x, this.points.get(i).y, linePaint);
+		}
+	}
+	
+	public void RotateDrawing(float ca, float sa, float x1, float y1)
+	{
+		float xp, yp;
+		for (int i=0; i<this.points.size(); i++) {
+			xp = (this.points.get(i).x - x1) * ca - (this.points.get(i).y - y1) * sa + x1;
+	        yp = (this.points.get(i).x - x1) * sa + (this.points.get(i).y - y1) * ca + y1;
+			
+	        this.points.set(i, new Point(this.points.get(i).x + xp, this.points.get(i).y + yp));
+		}
+	}
+	
+	public void addPoint(float x, float y) {
+		points.add(new Point(x, y));
+	}
+	
+}
---
+++ Project1/src/edu/msu/scrabble/project1/FinalActivity.java
@@ -0,0 +1,59 @@
+package edu.msu.scrabble.project1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class FinalActivity extends Activity {
+
+	private Game game;
+	
+	private TextView player1Name;
+	private TextView player2Name;
+	private TextView player1Score;
+	private TextView player2Score;
+	
+	private TextView winnerMessage;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_final);
+
+		Intent intent = getIntent();
+		game = (Game)intent.getSerializableExtra("GAME");
+		
+		int p1Score = game.getPlayer1Score();
+		int p2Score = game.getPlayer2Score();
+		
+		player1Name = (TextView)findViewById(R.id.textViewPlayer1);
+		player2Name = (TextView)findViewById(R.id.textViewPlayer2);
+		player1Score = (TextView)findViewById(R.id.textViewPlayer1Score);
+		player2Score = (TextView)findViewById(R.id.textViewPlayer2Score);
+		
+		winnerMessage = (TextView)findViewById(R.id.textViewWinner);
+		
+		player1Name.setText(game.getPlayer1Name());
+		player2Name.setText(game.getPlayer2Name());
+		
+		player1Score.setText(Integer.toString(p1Score));
+		player2Score.setText(Integer.toString(p2Score));
+		
+		if (p1Score > p2Score) {
+			winnerMessage.setText(game.getPlayer1Name() + " Wins!");
+		} else {
+			winnerMessage.setText(game.getPlayer2Name() + " Wins!");
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_final, menu);
+		return true;
+	}
+
+}
